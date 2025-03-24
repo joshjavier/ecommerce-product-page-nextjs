3cd1dbcd070979e5531ee3a2b6dd59188ec87ce4
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-03-24 17:00:16 +0800

    chore: update favicon and meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,9 @@ import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/c
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'Frontend Mentor | E-commerce product page',
+  description:
+    'A responsive e-commerce product page built with Next.js, featuring an interactive image gallery, cart functionality, and a modern UI.',
 };
 
 export default function RootLayout({ children }: { children: any }) {
@@ -14,7 +15,7 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en" {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
